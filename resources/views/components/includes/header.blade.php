da03af0b63272655a5b487a7c4093be3556b5d5f
Strayneko <bayurendi10@gmail.com>
2024-03-30 01:25:34 +0700

    Refac: remove unused code

--- resources/views/components/includes/header.blade.php
+++ resources/views/components/includes/header.blade.php
@@ -4,27 +4,7 @@
             <img src="assets/images/logo.svg" alt="Logo" class="w-32">
         </a>
 
-        <div class="w-full max-w-xl relative flex">
-                <span class="absolute left-4 top-3 text-lg text-gray-400">
-                    <i class="fa-solid fa-magnifying-glass"></i>
-                </span>
-            <input type="text" name="search" id="search"
-                   class="w-full border border-primary border-r-0 pl-12 py-3 pr-3 rounded-l-md focus:outline-none hidden md:flex"
-                   placeholder="Search">
-            <button
-                class="bg-primary md:items-center border border-primary text-white px-8 rounded-r-md hover:bg-transparent hover:text-primary transition hidden md:flex">Search</button>
-        </div>
-
         <div class="flex items-center space-x-4">
-            <a href="#" class="text-center text-gray-700 hover:text-primary transition relative">
-                <div class="text-2xl">
-                    <i class="fa-regular fa-heart"></i>
-                </div>
-                <div class="text-xs leading-3">Wishlist</div>
-                <div
-                    class="absolute right-0 -top-1 w-5 h-5 rounded-full flex items-center justify-center bg-primary text-white text-xs">
-                    8</div>
-            </a>
             <a href="#" class="text-center text-gray-700 hover:text-primary transition relative">
                 <div class="text-2xl">
                     <i class="fa-solid fa-bag-shopping"></i>
@@ -33,12 +13,6 @@
                 <div
                     class="absolute -right-3 -top-1 w-5 h-5 rounded-full flex items-center justify-center bg-primary text-white text-xs">
                     2</div>
-            </a>
-            <a href="#" class="text-center text-gray-700 hover:text-primary transition relative">
-                <div class="text-2xl">
-                    <i class="fa-regular fa-user"></i>
-                </div>
-                <div class="text-xs leading-3">Account</div>
             </a>
         </div>
     </div>
