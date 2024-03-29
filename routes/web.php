2bf327f6e5fdaf5a65497627c38d7d2bc145ad8c
Strayneko <bayurendi10@gmail.com>
2024-03-30 01:03:08 +0700

    Refac: Move home view to controller

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home.index');
-});
+Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::middleware([
     'auth:sanctum',
