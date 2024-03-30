ef6c719c0ab21e74bce711fde129dd8f6c8cfcf4
Strayneko <bayurendi10@gmail.com>
2024-03-31 04:25:05 +0700

    Feat: Create products factory

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $categoryIds = Category::query()->pluck('id')->toArray();
+        return [
+            'category_id' => $this->faker->numberBetween(min($categoryIds), max($categoryIds)),
+            'image_id' => 1,
+            'slug' => $this->faker->slug,
+            'name' => $this->faker->words(3, true),
+            'price' => $this->faker->numberBetween(100, 10000),
+            'stock' => $this->faker->numberBetween(0, 100),
+            'description' => $this->faker->paragraphs(2, true),
+            'is_published' => $this->faker->boolean(),
+        ];
+    }
+}
