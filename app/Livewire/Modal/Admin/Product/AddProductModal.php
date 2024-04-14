874ba4540908ba4188923cd64ab39282882d4b08
Strayneko <bayurendi10@gmail.com>
2024-04-15 00:42:26 +0700

    Feat: create add product form

--- app/Livewire/Modal/Admin/Product/AddProductModal.php
+++ app/Livewire/Modal/Admin/Product/AddProductModal.php
@@ -3,16 +3,51 @@
 namespace App\Livewire\Modal\Admin\Product;
 
 use App\Livewire\Forms\Admin\ProductForm;
+use App\Service\ProductService;
+use Livewire\Attributes\On;
+use Livewire\WithFileUploads;
 use LivewireUI\Modal\ModalComponent;
 
 class AddProductModal extends ModalComponent
 {
+    use WithFileUploads;
 
     public ProductForm $form;
 
+    private ProductService $productService;
+
+    public function boot()
+    {
+        $this->productService = new ProductService();
+    }
+
+    /**
+     * Generate unique slug based on product name when product name field updated
+     * @return void
+     */
+    public function updatedFormName(): void
+    {
+        $this->form->slug = $this->productService->generateSlug($this->form->name);
+    }
+
+    /**
+     * Store product
+     * @return void
+     */
     public function addProduct(): void
     {
+
         $this->form->validate();
+
+        $product = $this->productService->store($this->form);
+        if(is_null($product)) {
+            $this->dispatch('swal:message', message: 'Failed to store product. Please try again later.', title: 'Something went wrong!', type: 'error');
+            return;
+        }
+
+        $this->dispatch('swal:message', message: 'Product has been stored successfully.');
+        $this->closeModal();
+        $this->dispatch('product:added');
     }
 
     /** {@inheritDoc} */
@@ -20,6 +55,23 @@ class AddProductModal extends ModalComponent
     {
         return '3xl';
     }
+
+    /** {@inheritDoc} */
+    public static function dispatchCloseEvent(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Reset form when modalClosed event fired
+     * @return void
+     */
+    #[On('modalClosed')]
+    public function resetFormOnModalClosed(): void
+    {
+        $this->form->resetForm();
+    }
+
     public function render()
     {
         return view('livewire.modal.admin.product.add-product-modal');
--- resources/views/livewire/modal/admin/product/add-product-modal.blade.php
+++ resources/views/livewire/modal/admin/product/add-product-modal.blade.php
@@ -2,25 +2,30 @@
     <form action="" class="space-y-5" wire:submit.prevent="addProduct()">
         <div class="space-y-1">
             <x-label value="Name" required />
-            <x-input class="w-full" placeholder="Product Name" wire:model="form.name" />
+            <x-input class="w-full" placeholder="Product Name" wire:model.live.debounce="form.name" />
             <x-input-error for="form.name" />
         </div>
     <div class="space-y-1">
             <x-label value="Slug" required />
-            <x-input class="w-full" placeholder="product-name" />
+            <x-input class="w-full" placeholder="product-name" wire:model="form.slug" />
             <x-input-error for="form.slug" />
         </div>
 
         <div class="space-y-1">
             <x-label value="Price (RP)" required />
-            <x-input class="w-full" type="number" step="1" min="0" placeholder="10,000" />
+            <x-input class="w-full" type="number" step="1" min="0" placeholder="10,000" wire:model="form.price" />
             <x-input-error for="form.price" />
         </div>
 
         <div class="space-y-1">
             <x-label value="Stock" required />
-            <x-input class="w-full" type="number" step="1" min="0" placeholder="100" />
+            <x-input class="w-full" type="number" step="1" min="0" placeholder="100" wire:model="form.stock" />
             <x-input-error for="form.stock" />
+        </div>
+
+        <div class="space-y-1">
+            <x-label value="Description" required />
+            <textarea wire:model="form.description" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
         </div>
 
         <div>
@@ -34,10 +39,12 @@
 
     <x-slot:footer>
         <div class="flex justify-end gap-2.5">
-            <x-button class="bg-red-600 hover:bg-red-800 hover:text-white focus:bg-red-800 active:bg-red-800"
+            <x-button class="bg-red-600 hover:bg-red-800 hover:text-white focus:bg-red-800 active:bg-red-800 disabled:bg-gray-500 disabled:cursor-not-allowed"
+                      wire:loading.attr="disabled"
                       wire:click="$dispatch('closeModal')">Cancel</x-button>
             <x-button @click.prevent="$refs.addProductButton.click()"
-                      class="bg-blue-600 hover:bg-blue-800 hover:text-white focus:bg-blue-800 active:bg-blue-600"
+                      class="bg-blue-600 hover:bg-blue-800 hover:text-white focus:bg-blue-800 active:bg-blue-600 disabled:bg-gray-500 disabled:cursor-not-allowed"
+                      wire:loading.attr="disabled"
             >Add Product</x-button>
         </div>
     </x-slot:footer>
