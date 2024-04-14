746a4275635b571ea09342e6fd9867cf781f36bd
Strayneko <bayurendi10@gmail.com>
2024-04-15 00:37:33 +0700

    Feat(Chore): Add sweetalert2

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,8 @@
 import './bootstrap';
 import { initFlowbite } from "flowbite";
 import 'flowbite';
+import Swal from "sweetalert2";
+
 
 document.addEventListener("livewire:navigating", () => {
     // Mutate the HTML before the page is navigated away...
@@ -11,3 +13,11 @@ document.addEventListener("livewire:navigated", () => {
     // Reinitialize Flowbite components
     initFlowbite();
 });
+
+document.addEventListener('swal:message', (e) => {
+    Swal.fire({
+        title: e.detail.title || 'Congratulations!',
+        text: e.detail.message,
+        icon: e.detail.type || 'success',
+    });
+})
