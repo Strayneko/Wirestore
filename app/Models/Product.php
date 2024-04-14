5ca9f5f44abd9e506713b3951de771e582597e5d
Strayneko <bayurendi10@gmail.com>
2024-04-14 22:42:59 +0700

    Refac: add attribute to get product image and change image cardinality

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,15 +2,18 @@
 
 namespace App\Models;
 
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Cache;
 
 class Product extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes, Sluggable;
 
     protected $guarded = ['id'];
 
@@ -27,8 +30,29 @@ class Product extends Model
      * Relationship to get product image
      * @return HasOne
      */
-    public function image(): BelongsTo
+    public function image(): HasOne
     {
-        return $this->belongsTo(Images::class);
+        return $this->hasOne(Image::class);
+    }
+
+    public function productImage(): Attribute
+    {
+        return Attribute::get(
+                fn () => $this->image ?? Cache::get('default_image'),
+            );
+    }
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
     }
 }
