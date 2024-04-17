48ffa69d841a755471197442bbdca0fe1953340c
Strayneko <bayurendi10@gmail.com>
2024-04-17 23:24:00 +0700

    Feat: add sweetalert confirmation modal

--- resources/js/app.js
+++ resources/js/app.js
@@ -14,10 +14,33 @@ document.addEventListener("livewire:navigated", () => {
     initFlowbite();
 });
 
+
+/**
+ * ===============
+ * Sweetalert 2
+ * ===============
+ */
 document.addEventListener('swal:message', (e) => {
     Swal.fire({
         title: e.detail.title || 'Congratulations!',
         text: e.detail.message,
         icon: e.detail.type || 'success',
     });
+})
+
+
+document.addEventListener('swal:confirmation', (e) => {
+    Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        cancelButtonColor: "#3085d6",
+        confirmButtonColor: "#d33",
+        confirmButtonText: "Delete"
+      }).then((result) => {
+        if(!result.isConfirmed) return;
+
+        Livewire.dispatch(e.detail.eventDispatchName, {slug: e.detail.slug})
+      });
 })
