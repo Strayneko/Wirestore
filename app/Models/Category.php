147e3e420a7c0c55551ab4419dfa57913cecec00
strayneko <bayurendi10@gmail.com>
2024-05-12 21:45:30 +0700

    Feat: add category sluggable trait to category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,12 +2,39 @@
 
 namespace App\Models;
 
+use App\Observers\CategoryObserver;
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+#[ObservedBy(CategoryObserver::class)]
 class Category extends Model
 {
-    use HasFactory;
+    use HasFactory, Sluggable;
 
     protected $guarded = ['id'];
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
+
+    /**
+     *  Get the route key for the model.
+     * @return string
+     */
+    public function getRouteKeyName(): string
+    {
+        return 'slug';
+    }
 }
