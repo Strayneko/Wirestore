d1a3a0083b98d11a1f5e0476ad1c459be1a25591
strayneko <bayurendi10@gmail.com>
2024-05-12 21:40:05 +0700

    Refac: remove image id from category table

--- database/migrations/2024_03_29_182502_create_categories_table.php
+++ database/migrations/2024_03_29_182502_create_categories_table.php
@@ -13,9 +13,6 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('image_id')
-                ->constrained()
-                ->cascadeOnDelete();
             $table->string('name');
             $table->string('slug')
                 ->unique();
