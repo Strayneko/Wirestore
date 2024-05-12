5db4c3a8f447825fae79158204b21ed67d119f5d
strayneko <bayurendi10@gmail.com>
2024-05-12 21:43:21 +0700

    Feat: create add category modal

--- app/Livewire/Modal/Admin/Category/AddCategoryModal.php
+++ app/Livewire/Modal/Admin/Category/AddCategoryModal.php
@@ -2,19 +2,63 @@
 
 namespace App\Livewire\Modal\Admin\Category;
 
+use App\Livewire\Forms\Admin\Category\CategoryForm;
+use App\Models\Category;
+use App\Service\CategoryService;
+use App\Service\SlugService;
 use LivewireUI\Modal\ModalComponent;
 
 class AddCategoryModal extends ModalComponent
 {
+    public CategoryForm $form;
+
+    private SlugService $slugService;
+
+    private CategoryService $categoryService;
+
+    public function boot(): void
+    {
+        $this->slugService = new SlugService();
+        $this->categoryService = new CategoryService();
+    }
+
     public function mount()
     {
         $this->dispatch('modal-category-opened');
+    }
+
+    /**
+     * Generate slug from category name when category name updated
+     * @return void
+     */
+    public function updatedFormName(): void
+    {
+        $this->form->slug = $this->slugService->generateSlug($this->form->name, Category::class);
     }
 
     /** {@inheritDoc} */
     public static function modalMaxWidth(): string
     {
         return '2xl';
+    }
+
+    /**
+     * Save category to the database
+     * @return void
+     */
+    public function saveCategory()
+    {
+        $this->form->validate();
+        $category = $this->categoryService->store($this->form);
+
+        if(is_null($category)) {
+            $this->dispatch('swal:message', message: 'Failed to store category. Please try again later.', title: 'Something went wrong!', type: 'error');
+            return;
+        }
+
+        $this->dispatch('swal:message', message: 'Category has been stored successfully.');
+        $this->closeModal();
+        $this->dispatch('category:refresh');
     }
 
     public function render()
--- resources/views/livewire/modal/admin/category/add-category-modal.blade.php
+++ resources/views/livewire/modal/admin/category/add-category-modal.blade.php
@@ -1,28 +1,3 @@
 <x-modal.modal title="Add New Category">
-    <form action="" wire:submit.prevent="saveCategory()" class="space-y-4">
-        <div class="space-y-1">
-            <x-label value="Category Name" required />
-            <x-input class="w-full" placeholder="Category Name" wire:model.live.debounce.500ms="form.name" />
-            <x-input-error for="form.name" />
-        </div>
-
-        <div class="space-y-1">
-            <x-label value="Slug" required />
-            <x-input class="w-full" placeholder="category-name" wire:model="form.slug" />
-            <x-input-error for="form.slug" />
-        </div>
-    </form>
-
-    <x-slot:footer>
-        <div class="flex justify-end gap-2.5">
-            <x-button class="bg-red-600 hover:bg-red-800 hover:text-white focus:bg-red-800 active:bg-red-800 disabled:bg-gray-500 disabled:cursor-not-allowed"
-                      wire:loading.attr="disabled"
-                      wire:click="$dispatch('closeModal')">Cancel</x-button>
-            <x-button wire.click.prevent="saveCategory()"
-                      class="bg-blue-600 hover:bg-blue-800 hover:text-white focus:bg-blue-800 active:bg-blue-600 disabled:bg-gray-500 disabled:cursor-not-allowed"
-                      wire:loading.attr="disabled"
-                      wire:target="form.slug, saveCategory"
-            >Add Category</x-button>
-        </div>
-    </x-slot:footer>
+    @include('livewire.modal.admin.category.partials._category-form')
 </x-modal.modal>
