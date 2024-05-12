d837a8b0d4a9a0cdb6d6bcc96202730032959a12
strayneko <bayurendi10@gmail.com>
2024-05-12 21:46:56 +0700

    Feat: use category service to fetch categories

--- app/Livewire/Admin/Category/CategoryRecords.php
+++ app/Livewire/Admin/Category/CategoryRecords.php
@@ -2,7 +2,8 @@
 
 namespace App\Livewire\Admin\Category;
 
-use App\Models\Category;
+use App\Service\CategoryService;
+use Livewire\Attributes\On;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -10,9 +11,35 @@ class CategoryRecords extends Component
 {
     use WithPagination;
 
+    private CategoryService $categoryService;
+
+    public function boot(): void
+    {
+        $this->categoryService = new CategoryService();
+    }
+
+    /**
+     * Refresh product records when product:added event fired
+     * @return void
+     */
+    #[On('category:refresh')]
+    public function refreshProducts(): void
+    {
+        $this->dispatch('$refresh');
+    }
+
+    /**
+     * Refresh category record every paginator page changed
+     * @return void
+     */
+    public function updatingPaginators(): void
+    {
+        $this->dispatch('category:refresh');
+    }
+
     public function render()
     {
-        $categories = Category::query()->paginate(5);
+        $categories = $this->categoryService->fetchCategories();
         return view('livewire.admin.category.category-records', ['categories' => $categories]);
     }
 }
