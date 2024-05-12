a351fc34e79f8a209ce2e48793b24efcce92b7b8
strayneko <bayurendi10@gmail.com>
2024-05-12 21:47:57 +0700

    Feat: create category observer

---
+++ app/Observers/CategoryObserver.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Category;
+
+class CategoryObserver
+{
+    /**
+     * Handle the Category "creating" event.
+     */
+    public function creating(Category $category): void
+    {
+        $newCategoryName = cleanAndTitleizeString($category->name);
+        $category->name = $newCategoryName;
+    }
+
+    /**
+     * Handle the Category "updated" event.
+     */
+    public function updated(Category $category): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Category "deleted" event.
+     */
+    public function deleted(Category $category): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Category "restored" event.
+     */
+    public function restored(Category $category): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Category "force deleted" event.
+     */
+    public function forceDeleted(Category $category): void
+    {
+        //
+    }
+}
