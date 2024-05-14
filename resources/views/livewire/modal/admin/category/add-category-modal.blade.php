dee7f9e01144749d32813386022850de9794f683
strayneko <bayurendi10@gmail.com>
2024-05-14 09:08:49 +0700

    Refac: add button name type

--- resources/views/livewire/modal/admin/category/add-category-modal.blade.php
+++ resources/views/livewire/modal/admin/category/add-category-modal.blade.php
@@ -1,3 +1,3 @@
 <x-modal.modal title="Add New Category">
-    @include('livewire.modal.admin.category.partials._category-form')
+    @include('livewire.modal.admin.category.partials._category-form', ['type' => 'add'])
 </x-modal.modal>
