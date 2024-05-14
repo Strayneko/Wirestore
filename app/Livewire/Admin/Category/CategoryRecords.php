d55826ddc9395bc4a9240e6e645b15e703514463
strayneko <bayurendi10@gmail.com>
2024-05-14 09:09:58 +0700

    Feat: create delete category

--- app/Livewire/Admin/Category/CategoryRecords.php
+++ app/Livewire/Admin/Category/CategoryRecords.php
@@ -3,6 +3,7 @@
 namespace App\Livewire\Admin\Category;
 
 use App\Service\CategoryService;
+use Illuminate\Support\Facades\Log;
 use Livewire\Attributes\On;
 use Livewire\Component;
 use Livewire\WithPagination;
@@ -26,6 +27,36 @@ class CategoryRecords extends Component
     public function refreshProducts(): void
     {
         $this->dispatch('$refresh');
+    }
+
+    /**
+     * Handle event comes from confirmation box to delete the categirt
+     * @param string|null $slug
+     * @return void
+     */
+    #[On('category:delete')]
+    public function deleteCategory(?string $slug): void
+    {
+        if(is_null($slug)){
+            Log::error("Cannot proceed to delete the category slug is null.");
+            $this->dispatch('swal:message', title: 'Something went wrong!', message: 'Cannot delete this category, please try again later.', type: 'error');
+            return;
+        }
+
+        $delete = $this->categoryService->destroy($slug);
+
+        if(is_null($delete)){
+            $this->dispatch('swal:message', title: 'Something went wrong!', message: 'This category is not found..', type: 'error');
+            return;
+        }
+
+        if(!$delete){
+            $this->dispatch('swal:message', title: 'Internal server error!', message: 'Cannot delete this category at the moment, please try again later.', type: 'error');
+            return;
+        }
+
+        $this->dispatch('category:refresh');
+        $this->dispatch('swal:message', message: 'Category has been deleted successfully.');
     }
 
     /**
