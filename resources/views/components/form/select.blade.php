880bd644fe4d242a076193593fb7836455e8c787
strayneko <bayurendi10@gmail.com>
2024-05-14 09:53:32 +0700

    Feat: create select component

---
+++ resources/views/components/form/select.blade.php
@@ -0,0 +1,16 @@
+@props([
+    'id',
+    'label' => 'Select an option',
+    'model',
+    'required' => false,
+])
+<div class="space-y-1">
+    <x-label for="{{ $id }}" :value="$label" :required="$required" />
+    <select id="{{ $id }}"
+            wire:model="{{ $model }}"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+        {{ $slot }}
+    </select>
+
+    <x-input-error :for="$model" />
+</div>
