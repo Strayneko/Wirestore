e1767391c57747e7241df61acec8dac11c427bac
strayneko <bayurendi10@gmail.com>
2024-05-14 09:12:52 +0700

    Feat: create category service

--- app/Service/CategoryService.php
+++ app/Service/CategoryService.php
@@ -8,6 +8,11 @@ use Illuminate\Support\Facades\Log;
 
 class CategoryService {
 
+    /**
+     * Store category to db
+     * @param CategoryForm $data
+     * @return Category|null
+     */
     public function store(CategoryForm $data): ?Category
     {
         try{
@@ -25,6 +30,7 @@ class CategoryService {
         return $category;
     }
 
+
     /**
      * Fetch all categories
      * @return \Illuminate\Database\Eloquent\Collection
@@ -34,6 +40,12 @@ class CategoryService {
         return Category::query()->latest()->paginate(10);
     }
 
+    /**
+     * Update the given category with the given data
+     * @param Category|null $category
+     * @param CategoryForm $data
+     * @return Category|null
+     */
     public function update(?Category $category, CategoryForm $data): ?Category
     {
         if(is_null($category)) return null;
@@ -49,6 +61,40 @@ class CategoryService {
             Log::error($e->getMessage());
             return null;
         }
+    }
+    /**
+     * Find one category based on the given id
+     * @param int|null $id
+     * @return \App\Models\Category|null
+     */
+    public function findOne(?string $slug): ?\App\Models\Category
+    {
+        if(is_null($slug)) return null;
+
+        return Category::query()->where('slug', $slug)->first();
+    }
+
+    /**
+     * Delete category based on the given slug
+     * @param string|null $slug
+     * @return bool|null
+     */
+    public function destroy(?string $slug): ?bool
+    {
+        $category = $this->findOne($slug);
+
+        if (is_null($category)) {
+            Log::error("Failed to find category with slug = {$slug}.");
+            return null;
+        }
+        try{
+            return $category->delete();
+        }catch(\Exception $e){
+            Log::error($e->getMessage());
+
+            return null;
+        }
+
     }
 
 }
