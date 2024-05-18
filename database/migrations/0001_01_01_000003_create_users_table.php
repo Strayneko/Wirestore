b789f3cce18d1317a185f68e94302979dcf9108b
strayneko <bayurendi10@gmail.com>
2024-05-18 22:58:39 +0700

    Refac: add default user role for user table

--- database/migrations/0001_01_01_000003_create_users_table.php
+++ database/migrations/0001_01_01_000003_create_users_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_role_id')
+                ->default(\App\Enums\UserRoleType::User)
                 ->constrained()
                 ->cascadeOnDelete();
             $table->string('name');
